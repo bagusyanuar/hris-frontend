0941fffb53171f3448e2b65e66b497368f3dbbca
bagusyanuar <bagus.yanuar613@gmail.com>
2026-07-11 23:58:59 +0700

    fix(radio): resolve toggle state selection and correct label clickability in storybook
    - Use checked state and onclick handler to toggle group value
    
    - Fix Storybook wrapping issue by removing component property from defineMeta
    
    - Restore standard label for-attribute bindings and remove nested button containers

---
+++ src/lib/presentation/shared/components/radio/Radio.stories.svelte
@@ -0,0 +1,64 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Radio from './Radio.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Radio',
+		tags: ['autodocs']
+	});
+</script>
+
+<script lang="ts">
+	let selectedValue = $state('option1');
+</script>
+
+<Story name="Default Group">
+	<div class="flex flex-col gap-4">
+		<Radio 
+			bind:group={selectedValue} 
+			value="option1" 
+			label="Option One" 
+			description="This is the primary option." 
+		/>
+		<Radio 
+			bind:group={selectedValue} 
+			value="option2" 
+			label="Option Two" 
+			description="Choose this for secondary features." 
+		/>
+		<Radio 
+			bind:group={selectedValue} 
+			value="option3" 
+			label="Option Three" 
+			description="This is an alternative option." 
+		/>
+	</div>
+</Story>
+
+<Story name="With Validation Error">
+	<div class="flex flex-col gap-4">
+		<Radio 
+			bind:group={selectedValue} 
+			value="option1" 
+			label="Option One" 
+			error="Please select a different option."
+		/>
+	</div>
+</Story>
+
+<Story name="Disabled">
+	<div class="flex flex-col gap-4">
+		<Radio 
+			bind:group={selectedValue} 
+			value="option1" 
+			label="Disabled Unchecked" 
+			disabled
+		/>
+		<Radio 
+			group="option2" 
+			value="option2" 
+			label="Disabled Checked" 
+			disabled
+		/>
+	</div>
+</Story>
